8eb26696293590699285d5a46dec8956af6da7f7
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-09-30 10:21:19 +0800

    update

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,18 @@
 import styles from "./homepage.module.css";
+import Featured from "@/components/featured";
+import CategoryList from "@/components/categoryList";
+import CardList from "@/components/cardList";
+import Menu from "@/components/menu";
 
 export default function Home() {
-  return <div>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Featured />
+      <CategoryList />
+      <div className={styles.content}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
