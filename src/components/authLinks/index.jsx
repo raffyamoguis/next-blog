fdb2f61cd0ed680eba22841a612ea7f185d507ea
Raffy Amoguis <raffy.amoguis1@gmail.com>
2023-09-30 10:20:39 +0800

    added new components

---
+++ src/components/authLinks/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./authLinks.module.css";
+
+export default function AuthLinks() {
+  return <div className={styles.container}>index</div>;
+}
---
+++ src/components/themeToggle/index.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./themeToggle.module.css";
+
+export default function ThemeToggle() {
+  return <div className={styles.container}>index</div>;
+}
